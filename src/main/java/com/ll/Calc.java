14cfb53683c0c46f1cab76633acd3645963166ad
Aidennn-Lee <yw010804@gmail.com>
2023-10-30 11:10:51 +0900

    three of test case

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,10 @@
+package com.ll;
+
+public class Calc {
+    public static int run(String exp) {
+        if(exp.contains("-")){
+            return 0;
+        }
+        return 20;
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,15 +1,12 @@
 package com.ll;
+
+import static java.lang.Integer.parseInt;
+
 //재귀를 이용하여 풀 것.
 //Calc.run("3 * 1") == 3
-//Calc.run("3 * 1 + (1 - (4 * 1 - ( 1 - 1)))") == 0
+//Calc.run("3 * 1 + (1 - (4 * 1 - (1 - 1)))") == 0
 public class Main {
     public static void main(String[] args) {
-
-    }
-}
-
-class Calc{
-    run(String formula){
 
     }
 }
