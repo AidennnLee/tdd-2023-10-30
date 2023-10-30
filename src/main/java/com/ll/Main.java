e8c9a2df13a278bd22b498674cb72b044586a2ff
Aidennn-Lee <yw010804@gmail.com>
2023-10-30 09:39:49 +0900

    setting

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,15 @@
+package com.ll;
+//재귀를 이용하여 풀 것.
+//Calc.run("3 * 1") == 3
+//Calc.run("3 * 1 + (1 - (4 * 1 - ( 1 - 1)))") == 0
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Calc{
+    run(String formula){
+
+    }
+}
